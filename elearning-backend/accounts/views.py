abba91a1c873e12299ece1a8da486436b785826b
mdsoaibakh-eng <mdsoaibakh>
2026-02-07 12:43:35 +0530

    Initial project setup: Django backend + Next.js frontend

---
+++ elearning-backend/accounts/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from .models import User
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
